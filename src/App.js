0a5e44d17b5468139e376604697013ca4d8ec756
Vladyslava <fireflyingggg@gmail.com>
2023-09-05 13:43:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Weather from './components/Weather';
 import Forecast from './components/Forecast';
 
 function App() {
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState('Kharkiv');
   const [weather, setWeather] = useState({
-    temperature: '0',
+    ready: false,
   });
   const [showSearchForm, setShowSearchForm] = useState(false);
 
@@ -19,7 +19,21 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    search();
     toggleSearchForm();
+  };
+
+  const handleResponse = (response) => {
+    setWeather({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      pressure: response.data.temperature.pressure,
+      feelsLike: response.data.temperature.feels_like,
+    });
   };
 
   const handleMyLocationClick = () => {
@@ -38,6 +52,7 @@ function App() {
         },
         (error) => {
           alert('You must provide access to geolocation ❤');
+          console.error('You must provide access to geolocation ❤');
           setCity('Kharkiv');
         }
       );
@@ -47,38 +62,10 @@ function App() {
     }
   };
 
-  useEffect(() => {
-    handleMyLocationClick();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (city !== null) {
-          const response = await axios.get(
-            `https://api.shecodes.io/weather/v1/current?query=${city}&key=5t4badf2211oab190e2bd035f7fefd1a`
-          );
-          setWeather({
-            temperature: response.data.temperature.current,
-            description: response.data.condition.description,
-          });
-        }
-        if (city === null) {
-          setCity('Kharkiv');
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, [city]);
-
   const updateCity = (event) => {
     setCity(
       event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1)
     );
-    console.log(city);
   };
 
   const handleCityClick = async (selectedCity) => {
@@ -97,47 +84,62 @@ function App() {
     toggleSearchForm();
   };
 
-  return (
-    <div className="App">
-      <div className="container">
-        {showSearchForm ? (
-          <div className="sidebar">
-            <div className="sidebar-button">
-              <button onClick={toggleSearchForm}>
-                <MdOutlineClose />
-              </button>
+  const search = () => {
+    axios
+      .get(
+        `https://api.shecodes.io/weather/v1/current?query=${city}&key=5t4badf2211oab190e2bd035f7fefd1a&units=metric`
+      )
+      .then(handleResponse);
+  };
+
+  if (weather.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          {showSearchForm ? (
+            <div className="sidebar">
+              <div className="sidebar-button">
+                <button onClick={toggleSearchForm}>
+                  <MdOutlineClose />
+                </button>
+              </div>
+              <form onSubmit={handleSubmit} className="sidebar-search-form">
+                <input
+                  type="search"
+                  placeholder="&#x1F50E;  search location"
+                  onChange={updateCity}
+                ></input>
+                <button type="submit">Search</button>
+              </form>
+              <div className="sidebar-default-cities">
+                <button onClick={() => handleCityClick('Kharkiv')}>
+                  Kharkiv
+                </button>
+                <button onClick={() => handleCityClick('London')}>
+                  London
+                </button>
+                <button onClick={() => handleCityClick('Barcelona')}>
+                  Barcelona
+                </button>
+              </div>
             </div>
-            <form onSubmit={handleSubmit} className="sidebar-search-form">
-              <input
-                type="search"
-                placeholder="&#x1F50E;&#xFE0E;  search location"
-                onChange={updateCity}
-              ></input>
-              <button type="submit">Search</button>
-            </form>
-            <div className="sidebar-default-cities">
-              <button onClick={() => handleCityClick('Kharkiv')}>
-                Kharkiv
-              </button>
-              <button onClick={() => handleCityClick('London')}>London</button>
-              <button onClick={() => handleCityClick('Barcelona')}>
-                Barcelona
-              </button>
-            </div>
-          </div>
-        ) : (
-          <Weather
-            handleLocationClick={handleMyLocationClick}
-            toggleSearchForm={toggleSearchForm}
-            temperature={weather.temperature}
-            description={weather.description}
-            city={city}
-          />
-        )}
-        <Forecast />
+          ) : (
+            <Weather
+              handleLocationClick={handleMyLocationClick}
+              toggleSearchForm={toggleSearchForm}
+              temperature={weather.temperature}
+              description={weather.description}
+              city={city}
+            />
+          )}
+          <Forecast />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return 'Loading';
+  }
 }
 
 export default App;
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,18 +1,16 @@
+import { useState } from 'react';
 import ForecastOfTheDay from './ForecastOfTheDay';
 import styles from './Forecast.module.css';
 
 function Forecast() {
+  const [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(null);
+
   return (
     <div className={styles.main}>
       <div className={styles.mainButtons}></div>
       <div className={styles.mainContent}>
-        <div className={styles.mainForecast}>
-          <ForecastOfTheDay />
-          <ForecastOfTheDay />
-          <ForecastOfTheDay />
-          <ForecastOfTheDay />
-          <ForecastOfTheDay />
-        </div>
+        <div className={styles.mainForecast}></div>
         <div className={styles.mainHighlights}></div>
       </div>
       <footer></footer>
